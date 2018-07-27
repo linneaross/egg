feea7ccf415c9a78fb613f5afc8885507a76af20
Linnea Ross <linneaross@Linneas-MacBook-Pro-2.local>
2018-07-27 15:15:29 -0400

    Copied files from sheet2web.

---
+++ extractor/RuleInteraction.py
@@ -0,0 +1,60 @@
+class RuleSet(object):
+    def __init__(self, data):
+        self.ruleInteractions = data
+    def generateLanguageIndex(self, fileName):
+        langCatalog = {}
+        for R in self.ruleInteractions:
+            L = R.language
+            if L not in langCatalog:
+                langCatalog[L] = 0
+            langCatalog[L] += 1
+        sorted = langCatalog.keys()
+        sorted.sort()
+        for L in sorted:
+            print("Language: %s has %d records" % (L, langCatalog[L]))
+        
+    def generateInteractionIndex(self, fileName):
+        intCatalog = {}
+        for RT in self.ruleInteractions:
+            I = RT.opacityType
+            if I not in intCatalog:
+                intCatalog[I] = 0
+            intCatalog[I] += 1
+
+class RuleInteraction(object):
+    def __init__(self):
+        self.language = None
+        self.name = None
+        self.opacityType = None
+        self.references = None
+        self.freeVariation = None
+        self.comments = None
+        self.altAnalyses = None
+    def loadData(self, row):
+        for i in range(36 - len(row)):
+            row.append("")
+        self.language = row[3]
+        self.name = row[4]
+        self.opacityType = row[5]
+        self.references = row[6]
+        self.processes = []
+        self.processes.append(Process(row[7], row[8], row[14], row[15], row[16]))
+        self.processes.append(Process(row[17], row[18], row[24], row[25], row[26]))
+        self.freeVariation = row[29]
+        self.comments = row[31]
+        self.altAnalyses = row[32]
+    def toString(self):
+        result = 'Language: ' + self.language + '\nName: ' + self.name + '\nInteraction Type: ' + self.opacityType + '\nReferences: ' + self.references + "\nProcess 1: \n" + self.processes[0].toString() + '\nProcess 2: \n' + self.processes[1].toString() + '\nFree Variation? ' + self.freeVariation + '\nComments: ' + self.comments + '\nAlternate Analyses: ' + self.altAnalyses
+        return result;
+
+class Process(object):
+    def __init__(self, desc, ruleType, lim, prod, add):
+        self.description = desc
+        self.ruleType = ruleType
+        self.limitations = lim
+        self.productivity = prod
+        self.additionalInt = add
+    def toString(self):
+        result = 'Rule Description: ' + self.description + '\nRule Type: ' + self.ruleType + '\nLimitations: ' + self.limitations + '\nProductivity: ' + self.productivity + '\nAdditional Interactions: ' + self.additionalInt
+        return result;
+        
---
+++ extractor/getsheet.py
@@ -0,0 +1,86 @@
+import google.oauth2.credentials
+import json
+from RuleInteraction import RuleInteraction, RuleSet
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# TODO: You will need to change this to the location of your secrets file
+CLIENT_SECRETS_FILE = "/Users/linneaross/Downloads/client_secrets.json"
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+API_SERVICE_NAME = 'sheets'
+API_VERSION = 'v4'
+
+# The sheet-id is the identifier of the spreadsheet
+#
+# TODO: You will need to replace this with the ID of your spreadsheet
+#       It can be found from the URL of the spreadsheet in your browser (between slashes)
+SHEET_ID = '1BSEXfFzA2cRIMQGIrDmWrZnOcAHuVF9TBjRwtcq6_aQ'
+
+
+class SpreadSheetRow(object):
+    def __init__(self, apiRow):
+        self.values = []
+        for col in apiRow:
+            if "formattedValue" in col:
+                self.values.append(col['formattedValue'])
+            else:
+                self.values.append("")
+
+    def htmlRow(self):
+        result = "<tr>\n"
+        for value in self.values:
+            result += "  <td>%s</td>\n" % unicode(value)
+        result += "</tr>\n"
+        return result
+
+    
+class SpreadSheet(object):
+    def __init__(self, apiRowData):
+        #print json.dumps(apiRowData, indent=4, sort_keys=True)
+        self.rows = []
+        for row in apiRowData[1:]:
+            self.rows.append(SpreadSheetRow(row['values']))
+
+    def htmlTable(self):
+        result = "<table>\n"
+        for row in self.rows:
+            result += row.htmlRow()
+        result += "</table>"
+        return result
+
+
+def get_authenticated_service():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_local_server(host='localhost',
+                                        port=8080, 
+                                        success_message='You may close this window.',
+                                        open_browser=True)
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+
+def get_sheet(service, **kwargs):
+    results = service.spreadsheets().get(**kwargs).execute()
+    return results
+
+
+def extractCells(sheet):
+    return SpreadSheet(sheet['sheets'][0]['data'][0]['rowData'])
+
+
+if __name__ == '__main__':
+    service     = get_authenticated_service()
+    sheet       = get_sheet(service, spreadsheetId=SHEET_ID, includeGridData=True)
+    spreadsheet = extractCells(sheet)
+    rule_interactions = []
+    for row in spreadsheet.rows:
+        ri = RuleInteraction()
+        ri.loadData(row.values)
+        rule_interactions.append(ri)
+    rule_set = RuleSet(rule_interactions)
+    print ("Total number of records: %d" % len(rule_interactions))
+    rule_set.generateLanguageIndex("lang.html")
+    #for ri in rule_interactions:
+        #print (ri.toString())
