f4127540d483d8903c2a9f3b1d21df611e88d58d
Linnea Ross <linneaross@Linneas-MBP-2.home>
2018-08-05 20:53:12 -0400

    Finished rule layout

--- wapp/src/Detail.js
+++ wapp/src/Detail.js
@@ -11,7 +11,25 @@ class Detail extends Component {
   render() {
     return (
       <div className="Detail">
-      {this.state.rule.language}
+      <p>Language: {this.state.rule.language}</p>
+      <p>Interaction Name: {this.state.rule.name}</p>
+      <p>Interaction Type: {this.state.rule.opacityType}</p>
+      <p>Process 1:</p>
+      <p>Rule Type: {this.state.rule.processes[0].ruleType}</p>
+      <p>Description: {this.state.rule.processes[0].description}</p>
+      <p>Productivity: {this.state.rule.processes[0].productivity}</p>
+      <p>Limitations: {this.state.rule.processes[0].limitations}</p>
+      <p>Additional Infterpretations: {this.state.rule.processes[0].additionalInt}</p>
+      <p>Process 2:</p>
+      <p>Rule Type: {this.state.rule.processes[1].ruleType}</p>
+      <p>Description: {this.state.rule.processes[1].description}</p>
+      <p>Productivity: {this.state.rule.processes[1].productivity}</p>
+      <p>Limitations: {this.state.rule.processes[1].limitations}</p>
+      <p>Additional Infterpretations: {this.state.rule.processes[0].additionalInt}</p>
+      <p>References: {this.state.rule.references}</p>
+      <p>Free Variation: {this.state.rule.freeVariation}</p>
+      <p>Alternate Analyses: {this.state.rule.altAnalyses}</p>
+      <p>Comments: {this.state.rule.comments}</p>
       </div>
     );
   }
